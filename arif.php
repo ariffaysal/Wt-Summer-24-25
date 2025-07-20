b51c5c680cebb483c5c4ab7f3fab2f1716892d80
ariffaysal <130126431+ariffaysal@users.noreply.github.com>
2025-07-20 10:34:06 +0600

    Add files via upload

---
+++ arif.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+    
+    <title> this is the registration form  </title>
+    <body>
+       <center>
+       <h1 style = "color : blue ;">  AIUB</h1> <br>
+       <h2> REGISTRATION FORM </h2>
+       
+    
+       </center> 
+       <table>
+
+ <h2> Complete the registration </h2>
+
+    <tr>
+      <td>
+    Full Name:   <td><input type="text"> </td>
+       </tr>
+
+        
+    <tr>
+      <td>
+    Email:  </td> <td><input type="text"> </td>
+       </tr>
+       
+
+        
+    <tr>
+      <td>
+    Password:  </td> <td><input type="text"> </td>
+       </tr>
+       
+
+       
+ <tr>
+    <td> Gender:</td>
+    <td> <input type="radio" name = "des"> Male </td>
+    <td> <input type= "radio" name= "des"> Female</td>
+    <td> <input type= "radio" name= "des"> Others</td>
+ </tr>    
+ <tr>
+    <td> Language known:</td>
+    <td> <input type="checkbox" name = "des"> English </td>
+    <td> <input type= "checkbox" name= "des"> Bangla</td>
+    <td> <input type="checkbox" name = "des"> Hindi </td>
+ </tr> 
+
+
+<tr>
+   <td> Country:</td><td> <select input type= "dropbox"></td></td>
+   
+   
+</tr>
+
+
+
+<tr>
+ <td>
+Date Of Birth:</td><td><input type= "date"> </td>
+
+</tr>
+
+
+<tr>
+   <td> Upload photo:</td> <td><input type= "file"> </td>
+</tr>
+
+
+ 
+<tr>
+   <td>
+Comments: </td> <td><input type= "text" ></td>
+   </td>
+ </tr>
+
+
+ <tr>
+   <td></td>
+   <td><button type ="button">Register</buton></td>
+ </tr>
+
+ </table>
+    </body>
+</html>
