12676ea67a65dc9ffacbab2059efe38cabdb3a13
ariffaysal <ariffaysal001@gmai.com>
2025-07-26 19:51:51 +0600

    cmt

--- MID/LabTask2.php
+++ MID/LabTask2.php
@@ -1,1 +1,50 @@
+<!doctype html>
 
+
+<style>
+    h1{
+       
+        font-family: 'Times New Roman', Times, serif;
+
+    }
+
+    h2{
+     
+    }
+
+    p{
+        height: 20;
+        width: 10;
+        
+    }
+
+    div{
+
+    }
+
+
+    body{
+        background-color: skyblue;
+    }
+</style>
+
+
+
+<html>
+    <body>
+        <center>
+            
+        <title>Bank Management </title>
+        <h1> This Is AIUB Bank Management System</h1>
+        <h2>Quick Registration form </h2>
+        <p>where leaders are created</p>
+        </center>
+        
+        
+        
+
+
+
+
+    </body>
+</html>
