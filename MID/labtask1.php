3ce11dc1965e01cff798afa0398a07011c3804c5
MD ARIF <130126431+ariffaysal@users.noreply.github.com>
2025-07-26 09:13:27 +0600

    Create labtask1.php

---
+++ MID/labtask1.php
@@ -0,0 +1,44 @@
+ <!DOCTYPE html>
+
+<html>
+    <body>
+        <title>Webtech</title>
+        <center>
+            <h1> This is Webtech course</h1>
+          <h2>  <p style = "color: red;">This is not very hard</pstyle> </h2>
+        </center>
+<table>
+
+        <tr>
+            <td>Enter Your name : </td>  <td><input type="text"></td>
+        </tr>
+
+        <tr>
+            <td> Enter Email :<td> <input type="text"></td>
+        </tr>
+
+        <tr>
+            <td>Gender : </td> 
+             <td><input type="radio" name="des">Male</td>
+              <td><input type="radio" name="des">Female</td>
+        </tr>
+
+
+
+        <tr>
+            
+                <td>Language:</td>
+                <td> <input type="radio"> English</td>
+                <td> <input type="radio"> Bangla</td>
+
+            
+        </tr>
+
+        <tr>
+            <td>Date:</td> <td> <input type="date" ></td>
+        </tr>
+
+
+        </table>
+    </body>
+</html>
