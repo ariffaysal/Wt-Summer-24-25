126dd952c2ebc4142b222c893c0f42ed61615cc5
ariffaysal <ariffaysal001@gmai.com>
2025-08-03 10:42:39 +0600

    cmt

--- MID/labtask3/labtask3.js
+++ MID/labtask3/labtask3.js
@@ -9,37 +9,37 @@ function handleSubmit() {
     var state = document.getElementById("state").value;
     var contact = document.getElementById("contact").value;
 
-    if (!firstName || !lastName || !city || !state || !contact) {
+    if (firstName ==="" || lastName==="" || city==="" || state==="" || contact==="") {
         alert('Please fill in all fields.');
-        return;
+        return false;
     }
     if (!/^\d{11}$/.test(contact)) {
         alert('Please enter a valid 11-digit contact number.');
-        return;
+        return false;
     }
     if (!/^[a-zA-Z]+$/.test(firstName) || !/^[a-zA-Z]+$/.test(lastName)) {
         alert('First Name and Last Name should only contain letters.');
-        return;
+        return false;
     }
     if (!/^[a-zA-Z\s]+$/.test(city) || !/^[a-zA-Z\s]+$/.test(state)) {
         alert('City and State should only contain letters and spaces.');
-        return;
+        return false;
     }
     if (firstName.length < 3 || lastName.length < 3) {
         alert('First Name and Last Name should be at least 3 characters long.');
-        return;
+        return false;
     }
     if (city.length < 3 || state.length < 3) {
         alert('City and State should be at least 3 characters long.');
-        return;
+        return false;
     }
     if (contact.length !== 11) {
         alert('Contact number should be exactly 11 digits long.');
-        return;
+        return false;
     }
     if (!/^\d+$/.test(contact)) {
         alert('Contact number should only contain digits.');
-        return;
+        return false;
     }
     
 
